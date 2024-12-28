e7995acc236777685eeeab76e0ab35d21214fe64
Yeonji Gong <hellnow@naver.com>
2024-12-29 06:26:08 +0900

    complete

--- obsclock/script.js
+++ obsclock/script.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const clock = document.getElementById('clock');
+
+const urlParams = new URL(location.href).searchParams;
+const tzParams = urlParams.get('tz');
+const timeGetter = new Date();
+const timezoneOffset =  tzParams == null || isNaN(tzParams) ? 0 : timeGetter.getTimezoneOffset() + new Number(tzParams) * 60;
+
+setInterval(() => {
+    const now = new Date();
+    now.setMinutes(now.getMinutes() + timezoneOffset);
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}, 1000);
